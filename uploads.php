63eb2d6321d65c05a75da9cb40e9f29ce45b53b9
F-LX <bluedivingg@gmail.com>
2025-06-21 00:30:17 +0700

    Update uploads.php

--- uploads.php
+++ uploads.php
@@ -2,7 +2,7 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'] ?? '';
     if ($password === 'priadingjn') {
-        echo "Flag: CYC{anjaybowlehjuga}";
+        echo "Flag: CYC{jagonyabanksampesayamenggigil}";
     } else {
         echo "Password changed to: $password Coba lagi bwank!!";
     }
