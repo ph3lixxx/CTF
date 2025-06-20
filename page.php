026cc0c270472f6fd740555b0167441212d7d6b2
F-LX <bluedivingg@gmail.com>
2025-06-21 00:30:47 +0700

    Update page.php

--- page.php
+++ page.php
@@ -1,6 +1,6 @@
 <?php
 $page = $_GET['page'] ?? 'home';
-$flag = "CYC{wowngemrihbank}";
+$flag = "CYC{janganlupabersyukur}";
 
 if (preg_match('/flag/i', $page)) {
     echo "Nope bro, ga bisa akses langsung ke flag.";
